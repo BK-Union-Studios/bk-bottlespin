3829891ff175302f15a2d029aeb1a8ac458024a0
BK Union Studios <251369865+BK-Union-Studios@users.noreply.github.com>
2025-12-28 16:22:50 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,10 @@
 fx_version 'cerulean'
 game 'gta5'
+lua54 'yes'
 
-description 'BK Bottle Spin Script for qbx_core'
-version '1.0.0'
-author 'BK Union Studios'
+author 'BK Union Scripts'
+description 'BK Union - Bottlespin for [QBOX]'
+version '1.0.1'
 
 shared_scripts {
     '@ox_lib/init.lua',
@@ -16,6 +17,10 @@ client_scripts {
 
 server_scripts {
     'server/main.lua'
+}
+
+escrow_ignore {
+    'config.lua'   
 }
 
 dependencies {
